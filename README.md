140a15399ea598231e6974d3d4d79ba017902e3a
Vitaliy Biryuckov <stukov.biryuckov2017@yandex.ru>
2023-09-17 22:30:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WebDataBI
+ 
---
+++ src/main/java/ru/isu/math/config/DispatcherServletConfig.java
@@ -0,0 +1,20 @@
+package ru.isu.math.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {WebMVCConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
