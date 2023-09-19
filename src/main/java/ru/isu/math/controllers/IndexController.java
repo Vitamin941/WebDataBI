a43e17dba84886402bf3412cd150a698d01a74d9
Vitaliy Biryuckov <stukov.biryuckov2017@yandex.ru>
2023-09-19 18:36:02 +0800

    delete repositories package and decided to use DAO with jdbcTemplate (later might be simpleJDBC)

--- src/main/java/ru/isu/math/controllers/IndexController.java
+++ src/main/java/ru/isu/math/controllers/IndexController.java
@@ -3,21 +3,25 @@ package ru.isu.math.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.isu.math.dao.JdbcTemplateTest;
 import ru.isu.math.repositories.ModelTestRepository;
 
 @Controller
 public class IndexController {
 
-    public ModelTestRepository testRepository;
+//    public ModelTestRepository testRepository;
+
+    public final JdbcTemplateTest jdbcTemplateTest;
 
     @Autowired
-    IndexController(ModelTestRepository testRepository) {
-        this.testRepository = testRepository;
+    IndexController(JdbcTemplateTest jdbcTemplateTest) {
+//        this.testRepository = testRepository;
+        this.jdbcTemplateTest = jdbcTemplateTest;
     }
 
     @GetMapping
     public String loadIndex() {
-        long count = testRepository.count();
+        jdbcTemplateTest.test();
         return "index";
     }
 }
--- src/main/java/ru/isu/math/repositories/ModelTestRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.isu.math.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import ru.isu.math.model.Model;
-
-@Repository
-public interface ModelTestRepository extends CrudRepository<Integer, Model> {
-}
